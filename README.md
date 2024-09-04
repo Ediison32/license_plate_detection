3b0d9acd417c2b471e98dba7605e19dde82d70d1
Edison Alvarez <edisonalvarez32@gmail.com>
2024-09-04 02:02:52 -0500

    trainer yolov

---
+++ README.md
@@ -0,0 +1,25 @@
+
+
+# Nombre del Proyecto
+
+Detector de placas 
+
+## Instalación
+
+Necesita intalar algunas librerias como las mostradas en el archivo .txt para poder ejecutar.
+
+## Uso
+
+Despues de instaladas las librerias, se puede ejecutar el main.py para correr, tener encuenta que este no tiene video incluidos, es necesario 
+tener y agregarlo a la ruta.
+
+## Derechos de Autor
+
+ - Derechos de yolov
+
+## Contribuciones
+
+
+
+## Contacto
+
---
+++ main.py
@@ -0,0 +1,35 @@
+
+import matplotlib.pyplot as plt
+import cv2
+from ultralytics import YOLO
+
+
+if __name__ == '__main__':
+
+    cap = cv2.VideoCapture("../videos/carro2.MOV")  # mostrar video 
+
+    # cargar modelo 
+    model = YOLO("best_placa.pt")  
+    #mode = YOLO(best.pt) este modelo detecta carros y camiones 
+    """     
+    model.export(format="onnx")
+    onnx_model = YOLO("yolov8m.onnx")
+    results = onnx_model("https://ultralytics.com/images/bus.jpg") 
+    """
+    while cap.isOpened():
+        status, frame = cap.read()
+
+        if not status:
+            break
+        
+        #frame = cv2.resize(frame,(1920, 1080))
+        results = model(frame)
+
+        frame = results[0].plot()
+
+
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cap.release()
+
